51aa9c643db4968ee64f6fb6ce16a2378a80374c
Christian Kuhl <git@ckuhl.com>
2017-09-18 12:52:24 -0400

    [feature] Add command line verbosity switch
    - Add command line verbosity switch (from -v to -vvvv)
    - fix argument breakage

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3.5
+import argparse
 import logging
 import os
 import subprocess
@@ -7,9 +8,28 @@ import sys
 import yaml
 
 
+parser = argparse.ArgumentParser(
+        prog='pts - Python Test Suite',
+        description='Run plaintext tests against scripts')
+parser.add_argument(
+        '--verbose', '-v',
+        help='add verbosity',
+        action='count',
+        default=0)
+
+args = parser.parse_args()
+verb_log = {
+        0: logging.CRITICAL,
+        1: logging.ERROR,
+        2: logging.WARNING,
+        3: logging.INFO,
+        4: logging.DEBUG,
+}
+
 # set up logging
 log = logging.Logger(__name__)
-log.setLevel(logging.INFO)
+log.setLevel(verb_log[args.verbose])
+
 
 # gather files
 files = [ f for f in os.listdir( os.curdir ) if os.path.isfile(f) ]
@@ -32,11 +52,11 @@ for suite in testfile:
 
         # Check for arguments
         try:
-            arguments = test['args']
+            command = [*program, *test['args']]
         except KeyError:
-            arguments = ''
+            command = [*program]
 
-        run = subprocess.run([*program, test['args']],
+        run = subprocess.run(command,
                              input=test['in'].encode('utf-8'),
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
@@ -67,5 +87,5 @@ for suite in testfile:
         num_tests['total'] += 1
 
 
-log.warning(str(num_tests['total']) + ' tests run, with ' + str(num_tests['succeeded']) + ' successful and ' + str(num_tests['failed']) + ' failing')
+log.info(str(num_tests['total']) + ' tests run, with ' + str(num_tests['succeeded']) + ' successful and ' + str(num_tests['failed']) + ' failing')
 
