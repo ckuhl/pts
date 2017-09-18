b50665dde0baec9c924e47d93518492ea2950fd6
Christian Kuhl <git@ckuhl.com>
2017-09-18 11:37:50 -0400

    [feature] Add support for multiple YAML documents
    - add support for multiple YAML documents to allow for testing multiple
      scripts
    - add more logging for various levels of verbosity

--- README.md
+++ README.md
@@ -1,30 +1,59 @@
 # pts
 ## Python Test Suite
-A test suite that uses YAML files as tests.
+A test suite based on YAML files.
 
 ## Motivation
 Previously I have been using a bash script put together as an assignment for
 CS 246, however that has left me with a few annoyances. This is meant to solve
 those problems:
 
-- Having one file instead of 3 per test, for however many test cases there are.
-- Being difficult to extend
+- Having one file instead of 3 files for each test, multiplied by however many
+  test cases I have.
+- Being easy to break (as comes with little experience in shell scripting)
 
 ## Contents
 - `test.py` the testing script
-- `Testfile` the test cases
+- `Testfile` example test cases
 - `ex.py` an example script to test against
+
+## Usage
+    $ test.py
+    Testing: ['./ex.py'] ===========================================================
+    Test failed: Failing test
+      In: 1
+      Expected: 0
+      Out: 1
+    ================================================================================
+    5 tests run, with 4 successful and 1 failing
 
 ## Format
     ---
-    program: ./ex.py
+
+    program: [./ex.py]
     tests:
       - name: "Two plus two"
         in: "2 2"
         out: "4"
-      - name: "Two times two"
-        in: "2 2"
+
+      - name: "Multiplication"
+        in: "2 3"
         args: "-m"
-        out: "4"
+        out: "6"
+
+      - name: "Multiple addition"
+        in: "1 2 3 4 5"
+        out: "15"
+
+      - name: "Failing test"
+        in: "1"
+        out: "0"
+
+      - name: "Multiline test"
+        in: |
+         1
+         2
+         3
+        out: "6"
+
     ...
 
--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import yaml
 
 # set up logging
 log = logging.Logger(__name__)
+log.setLevel(logging.INFO)
 
 # gather files
 files = [ f for f in os.listdir( os.curdir ) if os.path.isfile(f) ]
@@ -16,26 +17,40 @@ testfile = None
 
 # check that a testfile is there
 if 'Testfile' in files:
-    testfile = yaml.load(open('Testfile', 'r'))
+    testfile = yaml.load_all(open('Testfile', 'r'))
 else:
     sys.exit()
 
+# test counts
+num_tests = {'succeeded': 0, 'failed': 0, 'total': 0}
+
 # loop through test cases
-program = testfile['program']
-for test in testfile['tests']:
-    try:
-        output = subprocess.run([*program, test['args']], stdout=subprocess.PIPE, input=test['in'].encode('utf-8'))
-    except KeyError:
-        output = subprocess.run([*program], stdout=subprocess.PIPE, input=test['in'].encode('utf-8'))
+for suite in testfile:
+    program = suite['program']
+    log.warning('Testing: ' + str(program) + ' ' + '=' * (80 - 1 - len('Testing: ' + str(program))))
+    for test in suite['tests']:
+        try:
+            output = subprocess.run([*program, test['args']], stdout=subprocess.PIPE, input=test['in'].encode('utf-8'))
+        except KeyError:
+            output = subprocess.run([*program], stdout=subprocess.PIPE, input=test['in'].encode('utf-8'))
 
-    output = output.stdout.decode('utf-8').strip()
+        output = output.stdout.decode('utf-8').strip()
 
-    if output != test['out']:
-        log.error('Test failed: ' + test['name'])
+        if output != test['out']:
+            log.error('Test failed: ' + test['name'])
 
-        log.warning('In:' + test['in'])
-        log.warning('Expected:' + test['out'])
-        log.warning('Out:' + output)
-    else:
-        log.debug('Test succeeded' + test['name'])
+            log.warning('  In: ' + test['in'])
+            log.warning('  Expected: ' + test['out'])
+            log.warning('  Out: ' + output)
+            log.error('=' * 80)
+
+            num_tests['failed'] += 1
+        else:
+            log.debug('Test succeeded: ' + test['name'])
+            log.debug('=' * 80)
+            num_tests['succeeded'] += 1
+        num_tests['total'] += 1
+
+
+log.warning(str(num_tests['total']) + ' tests run, with ' + str(num_tests['succeeded']) + ' successful and ' + str(num_tests['failed']) + ' failing')
 
