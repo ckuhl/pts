578af3fbebda82c2540b3e568261051318ce7711
Christian Kuhl <git@ckuhl.com>
2017-11-06 17:16:54 -0500

    [fix] Un-hardcode python path

--- pts.py
+++ pts.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3.5
+#!/usr/bin/env python3.5
+# pts - PythonTestSuite
+# Run a series of black box tests from a Testfile against a program
+
+
 import sys
 import pts
 sys.exit(pts.main())
